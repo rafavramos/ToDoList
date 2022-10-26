3a39ae96b4602b3f034ac9a28cde0d2d4eb661a6
Rafael Ramos <rafaelramos@unesc.net>
2022-10-26 13:34:50 -0300

    feat: Create Empty Task Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import styles from './App.module.css'
 import './global.css'
 import Header from './components/Header'
+import TaskBar from './components/TaskBar'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TaskBar />
     </div>
   )
 }
---
+++ src/components/TaskBar.tsx
@@ -0,0 +1,28 @@
+import styles from './TaskBar.module.css'
+import { ClipboardText } from 'phosphor-react'
+
+export default function TaskBar() {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.createdTasks}>
+          Tarefas Criadas <span>0</span>
+        </div>
+        <div className={styles.doneTasks}>
+          Concluídas <span>1 de 2</span>
+        </div>
+      </header>
+      <article className={styles.taskBoard}>
+        <div className={styles.taskBoardEmpty}>
+          <div className={styles.ClipboardText}>
+            <ClipboardText size={100} color="var(--gray-400)" weight="light" />
+          </div>
+          <p className={styles.p1}>Você ainda não tem tarefas cadastradas</p>
+          <p className={styles.p2}>
+            Crie tarefas e organize seus itens a fazer
+          </p>
+        </div>
+      </article>
+    </>
+  )
+}
